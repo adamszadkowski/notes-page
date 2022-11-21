23a70b7abe12db0e3f27ec17b4c660e37b9204a6
Adam Szadkowski <adam@szadkowski.info>
2022-11-21 21:38:50 +0100

    Configure sass

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,15 @@
 <template>
   <main>
-    <p>test</p>
+    <p>test <small>something</small></p>
   </main>
 </template>
+
+<style lang="scss" scoped>
+p {
+  font-size: 2em;
+
+  small {
+    color: rgba(168, 123, 34, 0.802);
+  }
+}
+</style>
