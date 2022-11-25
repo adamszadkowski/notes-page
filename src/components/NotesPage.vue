f9c0cc9cde7d51382ae3ce32d0df1725cafa50a0
Adam Szadkowski <adam@szadkowski.info>
2022-11-25 15:28:21 +0100

    Extract NotesPage component

---
+++ src/components/NotesPage.vue
@@ -0,0 +1,98 @@
+<template>
+    <section class="notes-page">
+        <div class="notes-page__note">
+            <div class="notes-page__icon notes-page__icon--red"></div>
+            <div class="notes-page__syllable notes-page__syllable--partial">Tu</div>
+        </div>
+        <div class="notes-page__note">
+            <div class="notes-page__icon notes-page__icon--orange"></div>
+            <div class="notes-page__syllable">taj</div>
+        </div>
+        <div class="notes-page__note">
+            <div class="notes-page__icon notes-page__icon--red"></div>
+            <div class="notes-page__syllable notes-page__syllable--partial">sy</div>
+        </div>
+        <div class="notes-page__note">
+            <div class="notes-page__icon notes-page__icon--green"></div>
+            <div class="notes-page__syllable notes-page__syllable--partial">la</div>
+        </div>
+        <div class="notes-page__note">
+            <div class="notes-page__icon notes-page__icon--orange"></div>
+            <div class="notes-page__syllable">by</div>
+        </div>
+        <div class="notes-page__note">
+            <div class="notes-page__icon notes-page__icon--red"></div>
+            <div class="notes-page__syllable notes-page__syllable--partial">bę</div>
+        </div>
+        <div class="notes-page__note">
+            <div class="notes-page__icon notes-page__icon--orange"></div>
+            <div class="notes-page__syllable">dą</div>
+        </div>
+        <div class="notes-page__note">
+            <div class="notes-page__icon notes-page__icon--green"></div>
+            <div class="notes-page__syllable">hej!</div>
+        </div>
+    </section>
+</template>
+  
+<style lang="scss" scoped>
+.notes-page {
+    width: 800px;
+    display: grid;
+    grid-template-columns: repeat(5, 1fr);
+    align-content: start;
+    row-gap: 15px;
+    border: 1px solid rgb(201, 201, 201);
+    padding: 30px;
+
+    &__note {
+        display: flex;
+        align-content: center;
+        align-items: center;
+        flex-direction: column;
+        margin-bottom: 5%;
+    }
+
+    &__icon {
+        border-radius: 50%;
+        width: 85%;
+
+        &::before {
+            content: "";
+            display: block;
+            padding-top: 100%;
+        }
+
+        &--red {
+            background-color: red;
+        }
+
+        &--orange {
+            background-color: orange;
+        }
+
+        &--green {
+            background-color: green;
+        }
+    }
+
+    &__syllable {
+        margin-top: 15px;
+        font-size: 1.5em;
+        text-align: center;
+        width: 100%;
+
+        &--partial {
+            position: relative;
+
+            &::after {
+                content: "-";
+                position: absolute;
+                width: 10px;
+                right: -5px;
+            }
+        }
+    }
+}
+</style>
+  
