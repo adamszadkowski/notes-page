aa9c6de50f0c25e6f5c3b92c97cb6de468c35c5a
Adam Szadkowski <adam@szadkowski.info>
2022-11-25 15:46:35 +0100

    Extract SingleNote component

--- src/components/NotesPage.vue
+++ src/components/NotesPage.vue
@@ -1,40 +1,27 @@
 <template>
     <section class="notes-page">
-        <div class="notes-page__note">
-            <div class="notes-page__icon notes-page__icon--red"></div>
-            <div class="notes-page__syllable notes-page__syllable--partial">Tu</div>
-        </div>
-        <div class="notes-page__note">
-            <div class="notes-page__icon notes-page__icon--orange"></div>
-            <div class="notes-page__syllable">taj</div>
-        </div>
-        <div class="notes-page__note">
-            <div class="notes-page__icon notes-page__icon--red"></div>
-            <div class="notes-page__syllable notes-page__syllable--partial">sy</div>
-        </div>
-        <div class="notes-page__note">
-            <div class="notes-page__icon notes-page__icon--green"></div>
-            <div class="notes-page__syllable notes-page__syllable--partial">la</div>
-        </div>
-        <div class="notes-page__note">
-            <div class="notes-page__icon notes-page__icon--orange"></div>
-            <div class="notes-page__syllable">by</div>
-        </div>
-        <div class="notes-page__note">
-            <div class="notes-page__icon notes-page__icon--red"></div>
-            <div class="notes-page__syllable notes-page__syllable--partial">bę</div>
-        </div>
-        <div class="notes-page__note">
-            <div class="notes-page__icon notes-page__icon--orange"></div>
-            <div class="notes-page__syllable">dą</div>
-        </div>
-        <div class="notes-page__note">
-            <div class="notes-page__icon notes-page__icon--green"></div>
-            <div class="notes-page__syllable">hej!</div>
-        </div>
+        <single-note color="red" syllable="Tu" :is-partial="true" />
+        <single-note color="orange" syllable="taj" :is-partial="false" />
+        <single-note color="red" syllable="sy" :is-partial="true" />
+        <single-note color="green" syllable="la" :is-partial="true" />
+        <single-note color="orange" syllable="by" :is-partial="false" />
+        <single-note color="red" syllable="bę" :is-partial="true" />
+        <single-note color="orange" syllable="dą" :is-partial="false" />
+        <single-note color="green" syllable="hej" :is-partial="false" />
     </section>
 </template>
-  
+
+<script lang="ts">
+import SingleNote from "@/components/SingleNote.vue";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+    components: {
+        SingleNote,
+    },
+});
+</script>
+
 <style lang="scss" scoped>
 .notes-page {
     width: 800px;
@@ -44,55 +31,5 @@
     row-gap: 15px;
     border: 1px solid rgb(201, 201, 201);
     padding: 30px;
-
-    &__note {
-        display: flex;
-        align-content: center;
-        align-items: center;
-        flex-direction: column;
-        margin-bottom: 5%;
-    }
-
-    &__icon {
-        border-radius: 50%;
-        width: 85%;
-
-        &::before {
-            content: "";
-            display: block;
-            padding-top: 100%;
-        }
-
-        &--red {
-            background-color: red;
-        }
-
-        &--orange {
-            background-color: orange;
-        }
-
-        &--green {
-            background-color: green;
-        }
-    }
-
-    &__syllable {
-        margin-top: 15px;
-        font-size: 1.5em;
-        text-align: center;
-        width: 100%;
-
-        &--partial {
-            position: relative;
-
-            &::after {
-                content: "-";
-                position: absolute;
-                width: 10px;
-                right: -5px;
-            }
-        }
-    }
 }
 </style>
-  
