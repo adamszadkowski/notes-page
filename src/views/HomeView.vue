25ae27b9fd08b5f73098d6fa6f75e9cd0c5a96cc
Adam Szadkowski <adam@szadkowski.info>
2022-11-26 17:49:08 +0100

    Remove list from main page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,5 @@
 <template>
   <main>
-    <nav>
-      <ul>
-        <li v-for="song in songs" :key="song.id">
-          <button @click="selectSong(song.id)">{{ song.title }}</button>
-        </li>
-      </ul>
-    </nav>
     <section class="song">
       <song-title></song-title>
       <notes-page class="notes-page" :song="currentSong"></notes-page>
@@ -30,28 +23,28 @@ export default defineComponent({
     SongTitle,
   },
   computed: {
-    songs() {
-      return this.store.songs;
-    },
     currentSong() {
       return this.store.currentSong;
-    },
-  },
-  methods: {
-    selectSong(id: string) {
-      this.store.selectSongById(id);
     },
   },
 });
 </script>
 
 <style lang="scss" scoped>
-section.song {
+main {
   display: flex;
   flex-direction: column;
   align-items: center;
 
-  border: 1px solid rgb(201, 201, 201);
-  max-width: 800px;
+  section.song {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    border: 1px solid rgb(201, 201, 201);
+    max-width: 800px;
+    width: 100%;
+    background: rgb(251, 253, 253);
+  }
 }
 </style>
