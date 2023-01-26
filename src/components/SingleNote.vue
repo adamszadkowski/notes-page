2ae5df0cad32bffda6d6095a8e117c7f07379a61
Adam Szadkowski <adam@szadkowski.info>
2023-01-26 18:18:27 +0100

    Use svg icons for up and down

--- src/components/SingleNote.vue
+++ src/components/SingleNote.vue
@@ -2,8 +2,17 @@
   <div>
     <base-note :note="strippedNote" :is-partial="isPartial">
       <template #content>
-        <div :class="colorClass">
-          <div class="notes-page__desc--fill" :style="colorStyle"></div>
+        <div class="notes-page__desc--arrow">
+          <svg viewBox="0 0 512 512">
+            <path
+              v-if="note.endsWith('+')"
+              d="M128.4,189.3L233.4,89c5.8-6,13.7-9,22.4-9c8.7,0,16.5,3,22.4,9l105.4,100.3c12.5,11.9,12.5,31.3,0,43.2 c-12.5,11.9-32.7,11.9-45.2,0L288,184.4v217c0,16.9-14.3,30.6-32,30.6c-17.7,0-32-13.7-32-30.6v-217l-50.4,48.2  c-12.5,11.9-32.7,11.9-45.2,0C115.9,220.6,115.9,201.3,128.4,189.3z"
+            />
+            <path
+              v-if="note.endsWith('-')"
+              d="M383.6,322.7L278.6,423c-5.8,6-13.7,9-22.4,9c-8.7,0-16.5-3-22.4-9L128.4,322.7c-12.5-11.9-12.5-31.3,0-43.2 c12.5-11.9,32.7-11.9,45.2,0l50.4,48.2v-217c0-16.9,14.3-30.6,32-30.6c17.7,0,32,13.7,32,30.6v217l50.4-48.2  c12.5-11.9,32.7-11.9,45.2,0C396.1,291.4,396.1,310.7,383.6,322.7z"
+            />
+          </svg>
         </div>
       </template>
       <template #syllable>{{ syllable }}</template>
@@ -35,24 +44,6 @@ export default defineComponent({
         return this.note.slice(0, -1);
       return this.note;
     },
-    colorClass() {
-      return [
-        ...(this.note.endsWith("+")
-          ? ["notes-page__desc", "notes-page__desc--high"]
-          : []),
-        ...(this.note.endsWith("-")
-          ? ["notes-page__desc", "notes-page__desc--low"]
-          : []),
-      ];
-    },
-    color() {
-      return this.store.getColor(this.note.charAt(0)) ?? "gray";
-    },
-    colorStyle() {
-      return {
-        "background-color": this.color,
-      };
-    },
   },
 });
 </script>
@@ -69,18 +60,17 @@ export default defineComponent({
     width: 40%;
     height: 40%;
 
-    &--fill {
-      width: 100%;
-      height: 100%;
-      transform: scale(105%);
-    }
+    &--arrow {
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translateX(-50%) translateY(-50%);
+      width: 80%;
+      height: 80%;
 
-    &--low {
-      transform: translateX(-50%) translateY(60%) rotate(45deg);
-    }
-
-    &--high {
-      transform: translateX(-50%) translateY(80%) rotate(225deg);
+      svg path {
+        fill: rgba(255, 255, 255, 0.925);
+      }
     }
   }
 }
